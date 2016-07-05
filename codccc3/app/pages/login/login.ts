e7c1af2bea9734c9390692f1b62e2195b7b773ba
임경현 <dlarudgus20@naver.com>
2016-07-05 18:55:49 +0900

    ionic onclick

--- codccc3/app/pages/login/login.ts
+++ codccc3/app/pages/login/login.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TabsPage} from '../tabs/tabs';
 
 @Component({
   templateUrl: 'build/pages/login/login.html'
 })
 export class LoginPage {
 
-  constructor() {
-    ;
+  constructor(private navController: NavController) {
+  }
+
+  onLoginTouch(event) {
+    this.navController.push(TabsPage);
   }
 }
